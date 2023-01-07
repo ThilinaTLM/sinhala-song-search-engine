f9c64dd8f7c64bb8679266b2d128b14abba43cf2
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2023-01-07 17:22:59 +0530

    deploy changes

--- song-manger-web/svelte.config.js
+++ song-manger-web/svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +9,13 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			strict: false
+		}),
 	}
 };
 
